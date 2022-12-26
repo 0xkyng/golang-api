15c918a21e797d9e035a50c233c0aa03b4077f5f
Isaac Wanger <teryima127@gmail.com>
2022-12-26 15:29:25 +0100

    search by Id

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -26,9 +27,52 @@ func getBooks(context *gin.Context) {
 }
 //////////////////////////////////////
 
+//////////////////////////////////////////////
+// Get book by id
+func getBookById(id string) (*book, error) {
+	for i, b := range books{
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found")
+}
+
+func bookById(context *gin.Context) {
+	id := context.Param("id")
+	book, err := getBookById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, book)
+
+}
+////////////////////////////////////////////
+
+////////////////////////////////////
+// POST Method
+func addBook(context *gin.Context) {
+	var newBook book
+
+	err := context.BindJSON(&newBook)
+	if err != nil{
+		return
+	}
+
+	books = append(books, newBook)
+
+	context.IndentedJSON(http.StatusCreated, newBook)
+}
+////////////////////////////////////
+
 
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.POST("/books", addBook)
+	router.GET("/books/:id", bookById)
 	router.Run("localhost:8080")
 }
